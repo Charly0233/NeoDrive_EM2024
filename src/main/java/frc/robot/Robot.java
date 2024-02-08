94782b2dcd46286aa940263ec4b9cbf92df7b9bf
DeadMan888 <sgilmayol8@gmail.com>
2024-02-08 16:03:27 -0600

    Controller_drive_10m

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,22 +5,29 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.DriveTrain;
+import frc.robot.ccommandds.Joysticks;
 
 public class Robot extends TimedRobot {
   
   private final XboxController controller = new XboxController(0);
   private final DriveTrain swerve = DriveTrain.getInstance();
-  private final double DEADBAND = 0;
+    private final Joysticks joysticks = new Joysticks(controller);
+    private final double DEADBAND = 0;
   private boolean FieldRelativeTeleop = true;
+
+  private Spark leftMotor1 = new Spark(0);
+  private Spark leftMotor2 = new Spark(1);
+  private Spark rightMotor1 = new Spark(2);
+  private Spark rightMotor2 = new Spark(3);
+
+  private Encoder encoder = new Encoder(0, 1, false, EncodingType.k4X);
+  private final double kDriveTick2Feet = 1.0 / 128 * 6 * Math.PI / 12;
+
 
   @Override
   public void robotInit() {
     SmartDashboard.putBoolean("FieldRelativeTeleop", FieldRelativeTeleop);
-  } 
 
-  @Override
-  public void robotPeriodic(){
-    swerve.periodic();
   } 
 
   @Override
@@ -29,6 +36,47 @@ public class Robot extends TimedRobot {
     if(FRT != FieldRelativeTeleop){ FieldRelativeTeleop = FRT;}
     driveWithController(FieldRelativeTeleop);
   }
+
+  @Override
+  public void autonomousInit() {
+    //encoder. 
+    encoder.reset();
+  }
+
+  final double kP = 0.5;
+
+  double setpoint = 0;
+
+  @Override
+  public void autonomousPeriodic() {
+    // get joystick command
+    if (joy1.getRawButton(1)) {
+      setpoint = 10;
+    } else if (joy1.getRawButton(2)) {
+      setpoint = 0;
+    }
+
+    double sensorPosition = encoder.get() * DriveTick2Feet;
+
+    // calculations
+    double error = setpoint - sensorPosition;
+
+    double outputSpeed = kP * error;
+
+    // output to motors
+    leftMotor1.set(outputSpeed);
+    leftMotor2.set(outputSpeed);
+    rightMotor1.set(-outputSpeed);
+    rightMotor2.set(-outputSpeed);
+  }
+
+  @Override
+  public void robotPeriodic(){
+    swerve.periodic(Start);
+    Joysticks.Start(controller.getAButton())
+    //Función para que entre en movimiento a 10 m 
+    
+  } 
 
   private void driveWithController(boolean fieldRelative){
     var xSpeed = MathUtil.applyDeadband(controller.getLeftX(), DEADBAND);
--- src/main/java/frc/robot/commands/Joysticks.java
+++ src/main/java/frc/robot/commands/Joysticks.java
@@ -1,3 +1,5 @@
+import edu.wpi.first.wpilibj.XboxController;
+
 public class Joysticks {
     //public class Joysticks extends commandBase {
     
@@ -6,19 +8,36 @@ public class Joysticks {
     import frc.robot.subsystems.ExampleSubsystem;
     import edu.wpi.first.wpilibj2.command.CommandBase;
     
-    public class ExampleCommand extends CommandBase {
+    public class Joysticks extends CommandBase {
       private final ExampleSubsystem m_subsystem;
+      private final XboxController m_controller;
+      public static final XboxController.Button kA
+
     
       /**
        * Creates a new ExampleCommand.
        *
        * @param subsystem The subsystem used by this command.
        */
-      public ExampleCommand(ExampleSubsystem subsystem) {
+      public Joysticks(ExampleSubsystem subsystem, XboxController controller) {
         m_subsystem = subsystem;
+        m_controller = controller;
+
+        System.out.println(m_controller);
         
         addRequirements(subsystem);
       }
+
+      
+      public Start(bool a) {
+        if(a) {
+          swerve.move()
+        }
+
+        kA = m_controller.getAButton(A);
+      }
+
+
     
       // Called when the command is initially scheduled.
       @Override
@@ -29,6 +48,7 @@ public class Joysticks {
       // Called every time the scheduler runs while the command is scheduled.
       @Override
       public void execute(double s) {
+        
         if (d>0.1)
          subsystem.move(s); 
         
